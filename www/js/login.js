6caa959f5a674d5dcd61b62133701ba2a6067d1d
Daniel Jácome <danicompu164@hotmail.com>
2018-10-17 21:30:46 -0500

    consumo de servicio

--- www/js/login.js
+++ www/js/login.js
@@ -2,13 +2,29 @@
 $(document).ready(function () {
 
     $("#btnaceptar").click(function () {
+
+        $.ajax({
+            url: 'http://vmi195100.contaboserver.net:8095/vlipcode-salud-ws-adm/webresources/login/obtenerPorCredencialesMovil',
+            type: 'POST',
+            data: JSON.stringify({ "login": "1818181818", "password": "4d6db86afa1528fbe5248db396b65119b7c177de" }),
+            dataType: 'json',
+            error: function (jqXHR, text_status, strError) {
+                alert('no connection ' + strError);
+            },
+            timeout: 90000,
+            async: false,
+            success: function (data) {
+
+                console.log("ingresa a conuslta");
+            }
+        });
         navegar();
     });
 
     function navegar() {
-        $("#panelNavegacion").load("citas.html");   
-                
+        $("#panelNavegacion").load("citas.html");
+
     }
 
-    
+
 });
